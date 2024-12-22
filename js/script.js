89fbac193cb88b70ab92c646683401d2bd313753
david cortez <david.cortez020@gmail.com>
2024-12-22 10:13:02 -0300

    css productos

--- js/script.js
+++ js/script.js
@@ -9,12 +9,13 @@ const getProducts = () => {
       const container = document.querySelector('.containerProducts')
       const productHTML = `
         <article class="product" data-id=${product.id}>
-          <img src="${product.imagen}" alt="${product.nombre}">
-          <div class="details">
-            <h3>${product.nombre}</h3>
-            <p>$${(product.precio).toFixed(3)}</p>
-            <a href="./details.html?id=${product.id}" class="btnDetails">Detalles</a>
-          </div>
+          <a href="./details.html?id=${product.id}">
+            <img src="${product.imagen}" alt="${product.nombre}">
+            <div class="details">
+              <h3>${product.nombre}</h3>
+              <p>$${(product.precio).toFixed(3)}</p>
+            </div>
+          </a>
         </article>`
       container.innerHTML += productHTML
     });
